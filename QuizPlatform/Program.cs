204fb7846d5800abd70f721bf8c8f95786e2883c
Олександр Семенюк <sashasemeniuk@MacBook-Pro-Oleksandr.local>
2025-06-05 17:28:46 +0300

    changed program

--- QuizPlatform/Program.cs
+++ QuizPlatform/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("hello world from semeniuk");
+Console.Write("hello");
+Console.ReadLine();
