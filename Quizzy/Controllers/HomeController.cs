c61a4d538cc0b87c9505808355692a2ae343cb93
George <utereskovygmail.com@MacBook-Pro-Yurii.local>
2025-06-11 12:57:02 +0300

    туц commit

--- Quizzy/Controllers/HomeController.cs
+++ Quizzy/Controllers/HomeController.cs
@@ -33,7 +33,11 @@ public class HomeController : Controller
 
     public IActionResult Contact()
     {
-        return View();
+        return View(new VerificationContactModel
+        {
+            IsSuccess = false,
+            Error = null
+        });
     }
 
     public IActionResult Search_Test()
@@ -60,6 +64,63 @@ public class HomeController : Controller
         }
 
         return View(results.ToList());
+    }
+
+
+
+    public IActionResult Contact_Submit(string Email, string Message, string Name)
+    {
+        try
+        {
+            SendEmail(Email, Name, Message);
+            return View("Contact", new VerificationContactModel
+            {
+                IsSuccess = true
+            });
+        }
+        catch (Exception ex)
+        {
+            return View("Contact", new VerificationContactModel
+            {
+                Error = "Something went wrong",
+                IsSuccess = false
+            });
+        }
+    }
+    
+    static void SendEmail(string email, string name, string message1)
+    {
+        var fromAddress = new MailAddress("quizzytests@gmail.com", "Quizzy");
+        var toAddress = new MailAddress("quizzytests@gmail.com");
+        const string fromPassword = "eplc imyv edac hxzn"; 
+        const string subject = "User Message";
+        string body = $"Message from {name} - {email}:\n{message1}";
+   
+        var smtp = new SmtpClient
+        {
+            Host = "smtp.gmail.com",
+            Port = 587, 
+            EnableSsl = true,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+        };
+   
+        using var message = new MailMessage(fromAddress, toAddress)
+        {
+            Subject = subject,
+            Body = body
+        };
+   
+        try
+        {
+            smtp.Send(message);
+            Console.WriteLine("Email sent successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
     }
 
 }
---
+++ Quizzy/Models/VerificationContactModel.cs
@@ -0,0 +1,7 @@
+namespace Quizzy.Models;
+
+public class VerificationContactModel
+{
+    public string Error { get; set; }
+    public bool IsSuccess { get; set; }
+}
