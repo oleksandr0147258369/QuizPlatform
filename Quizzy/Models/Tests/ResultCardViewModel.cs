cc9c79b4c25968596569ee7a80a593391bde6aa1
Vladislave34 <ostapchukvladislav77@gmail.com>
2025-06-25 22:29:50 +0300

    add result of tests

---
+++ Quizzy/Models/Tests/ResultCardViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Quizzy.Models.Tests
+{
+    public class ResultCardViewModel
+    {
+        public int ResultId { get; set; }
+        public int UserId { get; set; }        // Додано UserId
+        public int TestId { get; set; }        // Додано TestId
+
+        public string FullName { get; set; }
+        public int Points { get; set; }
+        public int TotalPoints { get; set; }
+
+        public int SuccessPercentage => TotalPoints == 0 ? 0 : (int)((double)Points / TotalPoints * 100);
+
+        public TimeSpan TimeSpent { get; set; }         // Час, витрачений на тест
+        public TimeSpan TimePerQuestion { get; set; }
+    }
+
+}
